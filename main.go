eb6ee6f6f55107f0ad9c6dd9af5b65cc14abfa24
Raven <alimateeee@gamil.com>
2025-12-09 17:52:55 +0330

    first importing

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/common-nighthawk/go-figure"
+	"os"
+)
+
+type Task struct {
+	name  string
+	date  string
+	state bool
+}
+
+func main() {
+	myFig := figure.NewColorFigure("CLI app", "", "blue", true)
+	myFig.Print()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run <program> arg1 arg2")
+		return
+	}
+
+	firstArg := os.Args[1]
+
+	switch firstArg {
+	case "--add":
+		{
+			AddTasks()
+		}
+	case "--edit":
+		{
+			EditTasks()
+		}
+	case "--del":
+		{
+			DeleteTasks()
+		}
+	default: {
+		fmt.Println("use one of our flag : --add , --edit , --del")
+		return
+	}
+	}
+	fmt.Println(firstArg)
+}
---
+++ tasks.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func AddTasks() {
+	var (
+		taskName string
+		taskDate string
+	)
+	fmt.Print("Task name : ")
+	fmt.Scan(&taskName)
+	fmt.Print("Task Date : ")
+	fmt.Scan(&taskDate)
+	
+
+	// creating a task.json file for storing the tasks
+}
+
+func EditTasks() {
+
+}
+
+func DeleteTasks() {
+
+}
